c2b9f2ac627d9579bf2559848055a1044d8f366a
J-Salt <js730674@sju.edu>
2022-11-17 21:18:09 -0500

    formatting

--- src/component/Clothes.js
+++ src/component/Clothes.js
@@ -1,110 +1,106 @@
-import React  from 'react';
-import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 //import navigation from '/Users/seanrock/Downloads/DripOrDrown/App.js'
 
-
 //import vlothes from './src/screens/Closet';
 
-
-export default function Clothes ({ name, type, weather,   }) {
-  
-      return (
-      
-      <View style={styles.list}>
-        <View style={styles.listingRatingContainer}>
-          
-          
+export default function Clothes({ name, type, weather }) {
+  return (
+    <View style={styles.list}>
+      <View style={styles.listingRatingContainer}></View>
+      <TouchableOpacity
+        style={styles.items}
+        onPress={() => navigation.navigate("Closet")}
+      >
+        <View style={styles.budgetTagsContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.type}>{type}</Text>
+          <Text style={styles.weather}>{weather}</Text>
         </View>
-        <TouchableOpacity style={styles.items}
-        onPress={() => navigation.navigate('Closet')}>
-          <View style={styles.budgetTagsContainer}>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.type}>{type}</Text>
-            <Text style={styles.weather}>{weather}</Text> 
-          </View>
-        </TouchableOpacity>
-        
-      </View>
-    )
-    
-  }
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   list: {
-    width: '100%',
-    flexDirection: 'column',
+    width: "100%",
+    flexDirection: "column",
     paddingHorizontal: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   itemsList: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-around'
+    flexDirection: "column",
+    justifyContent: "space-around",
   },
   itemtext: {
     fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center'
+    fontWeight: "bold",
+    textAlign: "center",
   },
   budgetTagsContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderBottomColor: 'black',
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomColor: "black",
     borderRadius: 4,
-    backgroundColor :'gray',
-    
+    backgroundColor: "gray",
   },
   items: {
-    borderColor: 'black',
+    borderColor: "black",
     borderRadius: 4,
-    backgroundColor :'gray',
-
+    backgroundColor: "gray",
   },
   name: {
-    width: '40%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    fontWeight: '100',
-    color: '#333333',
+    width: "40%",
+    flexDirection: "row",
+    alignItems: "center",
+    fontWeight: "100",
+    color: "#333333",
     fontSize: 15,
-    alignItems: 'left',
-    borderColor: 'black',
-    paddingLeft:25,
-    fontWeight: 'bold',
+    alignItems: "left",
+    borderColor: "black",
+    paddingLeft: 25,
+    fontWeight: "bold",
     paddingTop: 10,
     paddingBottom: 10,
   },
   type: {
-    width: '30%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    fontWeight: '100',
-    color: '#333333',
+    width: "30%",
+    flexDirection: "row",
+    alignItems: "center",
+    fontWeight: "100",
+    color: "#333333",
     fontSize: 15,
-    paddingHorizontal :10,
-    alignItems: 'center',
-    fontWeight: 'bold',
-    paddingLeft:25,
-    
+    paddingHorizontal: 10,
+    alignItems: "center",
+    fontWeight: "bold",
+    paddingLeft: 25,
   },
   weather: {
-    width: '30%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    fontWeight: '100',
-    color: '#333333',
+    width: "30%",
+    flexDirection: "row",
+    alignItems: "center",
+    fontWeight: "100",
+    color: "#333333",
     fontSize: 15,
-    paddingHorizontal :10,
-    alignItems: 'right',
-    fontWeight: 'bold',
-    
+    paddingHorizontal: 10,
+    alignItems: "right",
+    fontWeight: "bold",
   },
   budgetTagsText: {
-    fontWeight: '100',
-    color: 'gray',
+    fontWeight: "100",
+    color: "gray",
     fontSize: 15,
-    paddingHorizontal :10
+    paddingHorizontal: 10,
   },
 });
