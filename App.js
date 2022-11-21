1c030f28c8521270e47a61004375bcd27f564d7e
J-Salt <js730674@sju.edu>
2022-11-21 11:56:09 -0500

    added ForgotPassword navigator

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import {Node} from 'react';
-import {initializeApp} from 'firebase/app';
+import React, { useState } from "react";
+import { Node } from "react";
+import { initializeApp } from "firebase/app";
 import {
   getDocs,
   getFirestore,
   collection,
   firestore,
-} from 'firebase/firestore/lite';
+} from "firebase/firestore/lite";
 //import {firebase, db} from './firebase/firebase';
 import {
   SafeAreaView,
@@ -16,7 +16,7 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native';
+} from "react-native";
 
 import {
   Colors,
@@ -24,41 +24,39 @@ import {
   Header,
   LearnMoreLinks,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+} from "react-native/Libraries/NewAppScreen";
 
-import Clothes from './src/component/Clothes';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import Clothes from "./src/component/Clothes";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeMenu from './src/screens/HomeMenu';
-import Closet from './src/screens/Closet';
-import LoadIn from './src/screens/LoadIn';
-import LogIn from './src/screens/LogIn';
-import SignUp from './src/screens/SignUp';
-import Article from './src/screens/Article';
-
+import HomeMenu from "./src/screens/HomeMenu";
+import Closet from "./src/screens/Closet";
+import LoadIn from "./src/screens/LoadIn";
+import LogIn from "./src/screens/LogIn";
+import SignUp from "./src/screens/SignUp";
+import Article from "./src/screens/Article";
+import ForgotPassword from "./src/screens/ForgotPassword";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAF9QW9bvXKyWIiPpmaOgKunA51Jxe4iAw',
-  authDomain: 'dripordrown-90905.firebaseapp.com',
-  databaseURL: 'https://dripordrown-90905-default-rtdb.firebaseio.com',
-  projectId: 'dripordrown-90905',
-  storageBucket: 'dripordrown-90905.appspot.com',
-  messagingSenderId: '217796469697',
-  appId: '1:217796469697:web:3324196fa615c8c4f6c540',
-  measurementId: 'G-F0RSLNR2DY',
+  apiKey: "AIzaSyAF9QW9bvXKyWIiPpmaOgKunA51Jxe4iAw",
+  authDomain: "dripordrown-90905.firebaseapp.com",
+  databaseURL: "https://dripordrown-90905-default-rtdb.firebaseio.com",
+  projectId: "dripordrown-90905",
+  storageBucket: "dripordrown-90905.appspot.com",
+  messagingSenderId: "217796469697",
+  appId: "1:217796469697:web:3324196fa615c8c4f6c540",
+  measurementId: "G-F0RSLNR2DY",
 };
 
 const fbApp = initializeApp(firebaseConfig);
 const db = getFirestore(fbApp);
 const Stack = createStackNavigator();
 
-
-
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Section = ({ children, title }) => {
+  const isDarkMode = useColorScheme() === "dark";
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -67,7 +65,8 @@ const Section = ({children, title}) => {
           {
             color: isDarkMode ? Colors.white : Colors.black,
           },
-        ]}>
+        ]}
+      >
         {title}
       </Text>
       <Text
@@ -76,7 +75,8 @@ const Section = ({children, title}) => {
           {
             color: isDarkMode ? Colors.light : Colors.dark,
           },
-        ]}>
+        ]}
+      >
         {children}
       </Text>
     </View>
@@ -84,34 +84,32 @@ const Section = ({children, title}) => {
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const [checking,setIsChecking] = React.useState(true);
+  const [checking, setIsChecking] = React.useState(true);
   //console.log(users);
 
   return (
-
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="loginOriginal"
-            screenOptions={{
-            headerShown: false
-
-        }}>
-            <Stack.Screen name="LoadIn" component={LoadIn} />
-            <Stack.Screen name="LogIn" component={LogIn} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="HomeMenu" component={HomeMenu} />
-            <Stack.Screen name="Closet" component={Closet} />
-            <Stack.Screen name="Article" component={Article} />
-
-          </Stack.Navigator>
-        </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="loginOriginal"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="LoadIn" component={LoadIn} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="HomeMenu" component={HomeMenu} />
+        <Stack.Screen name="Closet" component={Closet} />
+        <Stack.Screen name="Article" component={Article} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 /*isSignedIn ? (
@@ -134,15 +132,15 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
 });
 
